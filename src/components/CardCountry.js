9b46658b9966f04a9af72abcb82cdcaff7338551
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-02-14 03:31:49 -0800

    create card component  for country

---
+++ src/components/CardCountry.js
@@ -0,0 +1,31 @@
+import Card from "react-bootstrap/Card";
+
+export default function CardCountry({
+  imgSrc,
+  title,
+  population,
+  region,
+  capital,
+}) {
+  return (
+    <Card style={{ width: "18rem" , border:"none" }} className="my-5">
+      <Card.Img variant="top" src={imgSrc} />
+      <Card.Body>
+        <Card.Title style={{ fontWeight: "800" }} className="my-4">
+          {title}
+        </Card.Title>
+        <Card.Text>
+          <span className="d-block my-1">
+            Population : <span className="text-secondary">{population}</span>
+          </span>
+          <span className="d-block my-1">
+            Region : <span className="text-secondary">{region}</span>
+          </span>
+          <span className="d-block my-1">
+            Capital : <span className="text-secondary">{capital}</span>
+          </span>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,35 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CardCountry from "../components/CardCountry";
 import "./Home.css";
 
 export default function Home() {
   return (
-    <section className="d-flex justify-content-between mt-4">
-      <form className="searchForm">
-        <button type="submit" className="border-0">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="magnify" />
-        </button>
-        <input
-          type="search"
-          className="border-0"
-          placeholder="search for a country..."
+    <>
+      <section className="d-flex justify-content-between mt-4 flex-wrap">
+        <form className="searchForm">
+          <button type="submit" className="border-0">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="magnify" />
+          </button>
+          <input
+            type="search"
+            className="border-0"
+            placeholder="search for a country..."
+          />
+        </form>
+        <select className="border-0 filter">
+          <option>Filter by Region</option>
+        </select>
+      </section>
+      <section className="d-flex justify-content-between mt-4 flex-wrap">
+        <CardCountry
+          imgSrc=""
+          title="USA"
+          population="81.234.222"
+          region="amrica"
+          capital="losAngeles"
         />
-      </form>
-      <select className="border-0 filter">
-        <option>Filter by Region</option>
-      </select>
-    </section>
+      </section>
+    </>
   );
 }
