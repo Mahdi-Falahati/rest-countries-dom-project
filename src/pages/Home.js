7b29fa0505f6eea4a9bbc4482e4599c803562b55
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-02-14 03:06:44 -0800

    add search and filter tag and style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Home.css";
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <section className="d-flex justify-content-between mt-4">
+      <form className="searchForm">
+        <button type="submit" className="border-0">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="magnify" />
+        </button>
+        <input
+          type="search"
+          className="border-0"
+          placeholder="search for a country..."
+        />
+      </form>
+      <select className="border-0 filter">
+        <option>Filter by Region</option>
+      </select>
+    </section>
+  );
 }
