79bb93412644d9cd891d0f70d098fb364b5f1a98
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-02-14 00:41:01 -0800

    import css file to app js file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import {
 import RootLayout from "./layouts/RootLayout";
 import Home from "./pages/Home";
 
+
+// css bootsrtap 
+import 'bootstrap/dist/css/bootstrap.css';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
