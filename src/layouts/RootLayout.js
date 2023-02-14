39562ce99a8ab7cb68516ecb2951c54b3569367d
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-02-14 01:39:54 -0800

    complete header with bootstrap

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,22 +1,43 @@
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Outlet, NavLink } from "react-router-dom";
 
 import "./RootLayout.css";
 
+// fontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+
+// bootstrap
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export default function RootLayout() {
   return (
     <>
-      <header className="header">
-        <nav className="navbar">
-          <NavLink to="/">Where in the world ? </NavLink>
-          <button>
-          <FontAwesomeIcon icon={faMoon} /> Dark Mode <FontAwesomeIcon icon="fa-regular fa-moon" />
-          </button>
-        </nav>
+      <header className="header py-2">
+        <Container>
+          <Row>
+            <Col>
+              <nav className="navbar">
+                <NavLink to="/">Where in the world ? </NavLink>
+                <button className="darkMode">
+                  <FontAwesomeIcon icon={faMoon} className="moonIcon" /> Dark
+                  Mode
+                </button>
+              </nav>
+            </Col>
+          </Row>
+        </Container>
       </header>
-      <main>
-        <Outlet />
+
+      <main className="main">
+        <Container>
+          <Row>
+            <Col>
+              <Outlet />
+            </Col>
+          </Row>
+        </Container>
       </main>
     </>
   );
