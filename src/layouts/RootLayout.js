6b7f7a723ede38d906e1b948e57e18d175cf58f2
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-02-13 23:57:45 -0800

    install font awesome

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,19 +1,23 @@
-import { Outlet, NavLink } from "react-router-dom"
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Outlet, NavLink } from "react-router-dom";
 
-import "./RootLayout.css"
+import "./RootLayout.css";
 
 export default function RootLayout() {
   return (
     <>
       <header className="header">
         <nav className="navbar">
-          <NavLink to="/">Where in the world ?</NavLink>
-          <button>Dark Mode</button>
+          <NavLink to="/">Where in the world ? </NavLink>
+          <button>
+          <FontAwesomeIcon icon={faMoon} /> Dark Mode <FontAwesomeIcon icon="fa-regular fa-moon" />
+          </button>
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
     </>
-  )
+  );
 }
