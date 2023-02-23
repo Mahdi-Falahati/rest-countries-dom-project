eee34b2638119dbafb5697f470a96a4ac17ae67d
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-02-23 04:01:17 -0800

    add link to all cards and add a litle style

--- src/components/CardCountry.js
+++ src/components/CardCountry.js
@@ -1,4 +1,7 @@
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+
+import "./CardCountry.css";
 
 export default function CardCountry({
   imgSrc,
@@ -8,24 +11,29 @@ export default function CardCountry({
   capital,
 }) {
   return (
-    <Card style={{ width: "15rem" , border:"none" }} className="my-5 mx-1">
-      <Card.Img variant="top" src={imgSrc} alt={title} />
-      <Card.Body>
-        <Card.Title style={{ fontWeight: "800" }} className="my-4">
-          {title}
-        </Card.Title>
-        <Card.Text>
-          <span className="d-block my-1">
-            Population : <span className="text-secondary">{population}</span>
-          </span>
-          <span className="d-block my-1">
-            Region : <span className="text-secondary">{region}</span>
-          </span>
-          <span className="d-block my-1">
-            Capital : <span className="text-secondary">{capital}</span>
-          </span>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <Link to={"/" + title} style={{ textDecoration: "none"}}>
+      <Card
+        style={{ width: "15rem", border: "none" }}
+        className="my-5 mx-1 cardCountry"
+      >
+        <Card.Img variant="top" src={imgSrc} alt={title} />
+        <Card.Body>
+          <Card.Title style={{ fontWeight: "800" }} className="my-4">
+            {title}
+          </Card.Title>
+          <Card.Text>
+            <span className="d-block my-1">
+              Population : <span className="text-secondary">{population}</span>
+            </span>
+            <span className="d-block my-1">
+              Region : <span className="text-secondary">{region}</span>
+            </span>
+            <span className="d-block my-1">
+              Capital : <span className="text-secondary">{capital}</span>
+            </span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 }
